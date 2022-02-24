5a0a0a27c60f0d110d05f0099f960019ba695e63
Isabel Sousa Cruz <isabel.sousa15246@gmail.com>
2022-02-24 11:27:20 +1100

    Added statistics about the cinema room and changed some functions that makes the program works better

--- src/cinema/Cinema.kt
+++ src/cinema/Cinema.kt
@@ -10,28 +10,36 @@ fun main() {
     val rows = readln().toInt()
     println("Enter the number of seats in each row:")
     val seatsPerRow = readln().toInt()
+    println()
     val cinemaSeats = MutableList(rows) { MutableList(seatsPerRow) { "S" } }
-
-    do {
-        val shouldExitMenu = showMenu(cinemaSeats)
-        println()
-    } while (!shouldExitMenu)
+    showMenu(cinemaSeats)
 }
 
-private fun showMenu(cinemaSeats: CinemaSeats): Boolean {
-    println(
-        """
-        1. Show the seats
-        2. Buy a ticket
-        0. Exit
-        """.trimIndent()
-    )
-    when (readln().toInt()) {
-        1 -> printSeats(cinemaSeats)
-        2 -> chooseSeat(cinemaSeats)
-        0 -> return true
-    }
-    return false
+private fun showMenu(cinemaSeats: CinemaSeats) {
+    var currentIncome = 0
+    do {
+        println(
+            """
+            1. Show the seats
+            2. Buy a ticket
+            3. Statistics
+            0. Exit
+            """.trimIndent()
+        )
+        var shouldExitMenu = false
+        val option = readln().toInt()
+        println()
+        when (option) {
+            1 -> printSeats(cinemaSeats)
+            2 -> {
+                val ticketValue = chooseSeatAndReturnPrice(cinemaSeats)
+                currentIncome += ticketValue
+            }
+            3 -> printStatistics(cinemaSeats, currentIncome)
+            0 -> shouldExitMenu = true
+        }
+        println()
+    } while (!shouldExitMenu)
 }
 
 private fun printSeats(cinemaSeats: CinemaSeats) {
@@ -48,23 +56,29 @@ private fun printSeats(cinemaSeats: CinemaSeats) {
     }
 }
 
-private fun chooseSeat(cinemaSeats: CinemaSeats) {
-    println("Enter a row number:")
-    val chosenRow = readln().toInt()
-    println("Enter a seat number in that row:")
-    val chosenSeat = readln().toInt()
+private fun chooseSeatAndReturnPrice(cinemaSeats: CinemaSeats): Int {
+    var ticketValue: Int? = null
+    do {
+        println()
+        println("Enter a row number:")
+        val chosenRow = readln().toInt()
+        println("Enter a seat number in that row:")
+        val chosenSeat = readln().toInt()
 
-    val rows = cinemaSeats.size
-    val seatsPerRow = cinemaSeats[0].size
-    if (chosenRow > rows || chosenSeat > seatsPerRow) {
-        println("Choose a valid seat")
-        return
-    }
-    cinemaSeats[chosenRow - 1][chosenSeat - 1] = "B"
-    val totalSeats = calculateTotalSeats(rows, seatsPerRow)
-    val calculateTicketValue = calculateTicketValue(chosenRow, totalSeats, rows)
-    println("Ticket price: $$calculateTicketValue")
-
+        val rows = cinemaSeats.size
+        val seatsPerRow = cinemaSeats[0].size
+        if (chosenRow > rows || chosenSeat > seatsPerRow) {
+            println("Wrong input!")
+        } else if (cinemaSeats[chosenRow - 1][chosenSeat - 1] == "B") {
+            println("That ticket has already been purchased!")
+        } else {
+            cinemaSeats[chosenRow - 1][chosenSeat - 1] = "B"
+            val totalSeats = calculateTotalSeats(rows, seatsPerRow)
+            ticketValue = calculateTicketValue(chosenRow, totalSeats, rows)
+            println("Ticket price: $$ticketValue")
+        }
+    } while (ticketValue == null)
+    return ticketValue
 }
 
 private fun calculateTotalSeats(rows: Int, seatsPerRow: Int): Int = seatsPerRow * rows
@@ -77,9 +91,34 @@ fun calculateTicketValue(chosenRow: Int, totalSeats: Int, rows: Int): Int {
     }
 }
 
-private fun calculateIncome(rows: Int, seatsPerRow: Int) {
+private fun printStatistics(cinemaSeats: CinemaSeats, currentIncome: Int) {
+    val purchasedSeats = cinemaSeats.calculatePurchasedSeats()
+    val rows = cinemaSeats.size
+    val seatsPerRow = cinemaSeats[0].size
+    println("Number of purchased tickets: $purchasedSeats")
+
     val totalSeats = calculateTotalSeats(rows, seatsPerRow)
-    val totalIncome = if (totalSeats < 60) {
+    val percentage = (100.0 * purchasedSeats) / totalSeats
+    println("Percentage: %.2f%%".format(percentage))
+
+    println("Current income: $$currentIncome")
+    val totalIncome = calculateTotalIncome(rows, seatsPerRow)
+    println("Total income: $$totalIncome")
+}
+
+private fun CinemaSeats.calculatePurchasedSeats(): Int {
+    var bookedSeats = 0
+    for (row in this) {
+        for (seat in row) {
+            if (seat == "B") bookedSeats++
+        }
+    }
+    return bookedSeats
+}
+
+private fun calculateTotalIncome(rows: Int, seatsPerRow: Int): Int {
+    val totalSeats = calculateTotalSeats(rows, seatsPerRow)
+    return if (totalSeats < 60) {
         totalSeats * TICKETS_NORMAL_VALUE
     } else {
         val totalRowsFrontHalf = rows / 2
@@ -88,7 +127,6 @@ private fun calculateIncome(rows: Int, seatsPerRow: Int) {
         val backHalfSeats = seatsPerRow * totalRowsBackHalf
         (frontHalfSeats * TICKETS_NORMAL_VALUE) + (backHalfSeats * TICKETS_BACKSEATS_VALUE)
     }
-    println("Total income:")
-    println("$$totalIncome")
 }
+
 
